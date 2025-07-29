90408d0d1554db04a3bf77cbfb06da307faf7ba8
raulhuauya <32596519+raulhuauya@users.noreply.github.com>
2025-07-29 12:23:53 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,20 @@
 import { defineConfig } from 'astro/config';
-    import tailwind from '@astrojs/tailwind';
-    import react from '@astrojs/react';
-    import sitemap from '@astrojs/sitemap';
-    import partytown from '@astrojs/partytown';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
-    // https://astro.build/config
-    export default defineConfig({
-        output: 'static',
-        site: 'https://tukuylabs.com',
-        integrations: [
-            tailwind(),
-            react(),
-            sitemap(),
-            partytown({
-                config: {
-                    forward: ["dataLayer.push"],
-                },
-            }),
-        ]
-    });
+export default defineConfig({
+    output: 'static',
+    site: 'https://tukuylabs.com',
+    integrations: [
+        tailwind(),
+        react(),
+        sitemap(),
+        partytown({
+            config: {
+                forward: ["dataLayer.push"],
+            },
+        }),
+    ]
+});
